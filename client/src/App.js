fdad7f2bb857076c81788fa8c78ea5c782291364
nitin <ntnbhat9@gmail.com>
2019-08-06 12:49:27 +0530

    intial setup 3

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nitin Bhat's React App coming soon
         </p>
         <a
           className="App-link"
