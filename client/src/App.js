c9cf4a56208f63ce7b4d80ea41bd9abce7e13dfd
nitin <ntnbhat9@gmail.com>
2019-08-06 18:10:26 +0530

    landing page completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import amazon from './amazon.jpg';
+import amazon from './amzon.png';
+import google from './google.png';
 import microsoft from './microsoft.png';
 import intuit from './intuit.png';
 import adobe from './adobe.png';
 import cisco from './cisco.jpeg';
+import fb from './fb.png';
+import walmart from './walmart.jpeg';
+
 import './App.css';
 import { Button,Navbar,Nav,NavDropdown,Col,Container,Row,Image} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
         <Navbar.Brand href="#home">
           <img
               alt=""
@@ -37,20 +41,36 @@ function App() {
       <header className="App-header">
        <Container>
         <Row>
-          <Col xs={7} md={3}>
-            <Image src={cisco} roundedCircle thumbnail fluid/>
+         <Col xs={6} md={3}>
+            <Image src={fb} roundedCircle thumbnail fluid/>
           </Col>
-          <Col xs={7} md={3}>
+          <Col xs={6} md={3}>
             <Image src={microsoft} roundedCircle thumbnail fluid/>
           </Col>
-          <Col xs={7} md={3}>
+          <br/><br/><br/>
+          <Col xs={6} md={3}>
             <Image src={intuit} roundedCircle thumbnail fluid/>
           </Col>
-          <Col xs={7} md={3}>
+          <Col xs={6} md={3}>
             <Image src={adobe} roundedCircle thumbnail fluid/>
+          </Col>
+          <br/><br/><br/>
+          <Col xs={6} md={3}>
+            <Image src={amazon} roundedCircle thumbnail fluid/>
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src={google} roundedCircle thumbnail fluid/>
+          </Col>
+          <br/><br/><br/><br/>
+          <Col xs={6} md={3}>
+            <Image src={cisco} roundedCircle thumbnail fluid/>
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src={walmart} roundedCircle thumbnail fluid/>
           </Col>
         </Row>
       </Container>
+    
        <Container> 
          <p>
           Want to ace the technical interviews?
